2d6d7acd269b8dba9c89b681861b06ab0dafd564
bambamboole <manuel.christlieb@xentral.com>
2025-01-18 22:41:41 +0100

    add more test todos

--- tests/Feature/MenuTest.php
+++ tests/Feature/MenuTest.php
@@ -1,9 +1,12 @@
 <?php
 
+it('needs permission to create menus', function () {})->todo();
 it('can create menus', function () {})->todo();
 
+it('needs permission to update menus', function () {})->todo();
 it('can update menus', function () {})->todo();
 
+it('needs permission to delete menus', function () {})->todo();
 it('can delete menus', function () {})->todo();
 
 it('can create menu items', function () {})->todo();
--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -10,3 +10,16 @@ it('shows posts index on /blog', function () {
 
     $this->get('/blog')->assertSeeText($posts->map->title->toArray());
 });
+
+it('shows only published posts', function () {})->todo();
+
+it('needs permission to create posts', function () {})->todo();
+it('can create posts', function () {})->todo();
+
+it('needs permission to update posts', function () {})->todo();
+it('can update posts', function () {})->todo();
+
+it('needs permission to delete posts', function () {})->todo();
+it('can delete posts', function () {})->todo();
+
+it('properly resizes the featured image', function () {})->todo();
